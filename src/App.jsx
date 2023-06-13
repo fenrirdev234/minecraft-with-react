1ddd6e24622b2befcec926eeb910f99578be643c
Ciro Sira <ciroalejandro1@gmail.com>
2023-06-12 22:33:02 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Sky } from "@react-three/drei"
+import {Canvas} from "@react-three/fiber"
+
+
+function App() {
+
+
+  return (
+    <div>
+  <Canvas>
+<Sky/>
+  </Canvas>
+    </div>
+  )
+}
+
+export default App
